bec4c1df3794578809946a6da9f07999bb7d72a1
ReivelTimothy <timothyreivel1@gmail.com>
2026-03-17 18:25:49 +0700

    Chore : add Utils

---
+++ server/src/utils/apiError.ts
@@ -0,0 +1,14 @@
+export class ApiError extends Error {
+  public statusCode: number;
+  public status: string;
+  public isOperational: boolean;
+
+  constructor(statusCode: number, message: string) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
